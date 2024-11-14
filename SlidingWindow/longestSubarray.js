b1520aad16cbf30dc332bfdd9d46d1e807c35806
Nazmul-Git <nazmul.hasan16021998@gmail.com>
2024-11-14 07:48:51 +0600

    Solve finding the longest subarray length by using sliding window.

---
+++ SlidingWindow/longestSubarray.js
@@ -0,0 +1,19 @@
+const longestSubarray=function(nums, k){
+    let l=0, r=0;
+    let sum = 0;
+    let maxLength=0;
+
+    while(r<nums.length-1){
+        sum += nums[r];
+        if(sum>k){
+            sum -= nums[l];
+            l++;
+        } 
+        else if(sum <= k){
+            maxLength = Math.max(maxLength, r-l+1);
+        }
+        r += 1;
+    }
+    return maxLength;
+}
+console.log(longestSubarray([2,5,1,1,1,5,1], 14))
